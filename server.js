45b14fbf4c85ffeda8e3299aee91992b4e461fa1
Phudhinan21 <134205036+Phudhinan21@users.noreply.github.com>
2023-10-01 03:38:46 +0700

    connected mysql

--- server.js
+++ server.js
@@ -3,14 +3,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const sequelize = require("./util/db");
+
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server start on port ${PORT}`);
-});
+sequelize
+  .sync()
+  .then((result) => {
+    app.listen(PORT, () => {
+      console.log(`Server start on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
